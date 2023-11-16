83c28dce1b4b7409bd0f48881b94db41b95fa22f
NunzioDiCroce <nunziodavide@gmail.com>
2023-11-16 15:33:12 +0100

    Create Spring Boot project

---
+++ YourPlace_BackEnd/YourPlace_BackEnd_App/src/main/java/Projects/YourPlace_BackEnd_App/YourPlaceBackEndAppApplication.java
@@ -0,0 +1,13 @@
+package Projects.YourPlace_BackEnd_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourPlaceBackEndAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourPlaceBackEndAppApplication.class, args);
+	}
+
+}
---
+++ YourPlace_BackEnd/YourPlace_BackEnd_App/src/test/java/Projects/YourPlace_BackEnd_App/YourPlaceBackEndAppApplicationTests.java
@@ -0,0 +1,13 @@
+package Projects.YourPlace_BackEnd_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourPlaceBackEndAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
