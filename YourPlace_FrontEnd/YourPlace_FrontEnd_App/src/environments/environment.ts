b2e46aa9a28c1caece98d998cb64a6910e2d43e4
NunzioDiCroce <nunziodavide@gmail.com>
2023-11-22 09:38:52 +0100

    Update environment.ts

--- YourPlace_FrontEnd/YourPlace_FrontEnd_App/src/environments/environment.ts
+++ YourPlace_FrontEnd/YourPlace_FrontEnd_App/src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  // - - - - - - baseURL definition - - - - - -
+  // baseURL: 'http://localhost:4200/'
+  baseURL: 'http://localhost:3001/'
+
 };
 
 /*
