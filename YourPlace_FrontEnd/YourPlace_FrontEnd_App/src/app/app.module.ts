348bbda6964718aded5b48e69ca72b34814ab9b3
NunzioDiCroce <nunziodavide@gmail.com>
2023-11-22 11:59:06 +0100

    Update app.module.ts

--- YourPlace_FrontEnd/YourPlace_FrontEnd_App/src/app/app.module.ts
+++ YourPlace_FrontEnd/YourPlace_FrontEnd_App/src/app/app.module.ts
@@ -19,6 +19,36 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { ProfileUpdateComponent } from './components/profile-update/profile-update.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { LoginComponent } from './auth/login/login.component';
+import { Route, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+// - - - - - - routes configuration - - - - - -
+const routes: Route[] = [
+  { path: '', component: HomeComponent },
+
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+
+  { path: 'reservations', component: ReservationsComponent, canActivate: [AuthGuard] },
+  { path: 'reservationCreate', component: ReservationCreateComponent, canActivate: [AuthGuard] },
+  { path: 'reservations/:id', component: ReservationDetailsComponent, canActivate: [AuthGuard] },
+  { path: 'reservationUpdate/:id', component: ReservationUpdateComponent, canActivate: [AuthGuard] },
+
+  { path: 'places', component: PlacesComponent, canActivate: [AuthGuard] },
+  { path: 'placeCreate', component: PlaceCreateComponent, canActivate: [AuthGuard] },
+  { path: 'places/:id', component: PlaceDetailsComponent, canActivate: [AuthGuard] },
+  { path: 'placeUpdate/:id', component: PlaceUpdateComponent, canActivate: [AuthGuard] },
+
+  { path: 'users', component: UsersComponent, canActivate: [AuthGuard] },
+  { path: 'users/:id', component: UserDetailsComponent, canActivate: [AuthGuard] },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
+  { path: 'profileUpdate', component: ProfileUpdateComponent, canActivate: [AuthGuard] },
+
+  { path: '**', redirectTo: ''}
+];
+
 
 @NgModule({
   declarations: [
@@ -42,7 +72,12 @@ import { LoginComponent } from './auth/login/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    // - - - - - - additional imports - - - - -
+    RouterModule.forRoot(routes),
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
