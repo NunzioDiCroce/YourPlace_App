ba73bf2a45ccb9a78fb75145d76e55d624143ef5
NunzioDiCroce <nunziodavide@gmail.com>
2023-11-24 17:05:31 +0100

    Create NotFoundException.java

---
+++ YourPlace_BackEnd/YourPlace_BackEnd_App/src/main/java/Projects/YourPlace_BackEnd_App/exceptions/NotFoundException.java
@@ -0,0 +1,9 @@
+package Projects.YourPlace_BackEnd_App.exceptions;
+
+public class NotFoundException extends RuntimeException {
+
+	public NotFoundException(String message) {
+		super(message);
+	}
+
+}
