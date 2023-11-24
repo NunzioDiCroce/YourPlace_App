16f9f4b187429cb6a438f8dc949a95e85b037b13
NunzioDiCroce <nunziodavide@gmail.com>
2023-11-24 12:19:59 +0100

    Create Services

--- YourPlace_BackEnd/YourPlace_BackEnd_App/src/main/java/Projects/YourPlace_BackEnd_App/services/ReservationService.java
+++ YourPlace_BackEnd/YourPlace_BackEnd_App/src/main/java/Projects/YourPlace_BackEnd_App/services/ReservationService.java
@@ -1,8 +1,49 @@
 package Projects.YourPlace_BackEnd_App.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+
+import Projects.YourPlace_BackEnd_App.entities.Reservation;
+import Projects.YourPlace_BackEnd_App.repositories.ReservationRepository;
 
 @Service
 public class ReservationService {
+
+	private final ReservationRepository reservationRepository;
+
+	@Autowired
+	public ReservationService(ReservationRepository reservationRepository) {
+		this.reservationRepository = reservationRepository;
+	}
+
+	// - - - - - - createReservation - - - - - -
+	public Reservation createReservation() {
+		return null;
+
+	}
+
+	// - - - - - - getReservations - - - - - -
+	public Page<Reservation> getReservations() {
+		return null;
+
+	}
+
+	// - - - - - - findReservation - - - - - -
+	public Reservation findReservation() {
+		return null;
+
+	}
+
+	// - - - - - - updateReservation - - - - - -
+	public Reservation updateReservation() {
+		return null;
+
+	}
+
+	// - - - - - - deleteReservation - - - - - -
+	public void deleteReservation() {
+
+	}
 
 }
--- YourPlace_BackEnd/YourPlace_BackEnd_App/src/main/java/Projects/YourPlace_BackEnd_App/services/UserService.java
+++ YourPlace_BackEnd/YourPlace_BackEnd_App/src/main/java/Projects/YourPlace_BackEnd_App/services/UserService.java
@@ -1,8 +1,49 @@
 package Projects.YourPlace_BackEnd_App.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+
+import Projects.YourPlace_BackEnd_App.entities.User;
+import Projects.YourPlace_BackEnd_App.repositories.UserRepository;
 
 @Service
 public class UserService {
+
+	private final UserRepository userRepository;
+
+	@Autowired
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	// - - - - - - createUser - - - - - -
+	public User createUser() {
+		return null;
+
+	}
+
+	// - - - - - - getUsers - - - - - -
+	public Page<User> getUsers() {
+		return null;
+
+	}
+
+	// - - - - - - findUser - - - - - -
+	public User findUser() {
+		return null;
+
+	}
+
+	// - - - - - - updateUser - - - - - -
+	public User updateUser() {
+		return null;
+
+	}
+
+	// - - - - - - deleteUser - - - - - -
+	public void deleteUser() {
+
+	}
 
 }
