a73af6e3dc8f7b91fbdbfb64bbc1356268c2b983
NunzioDiCroce <nunziodavide@gmail.com>
2023-11-24 12:55:22 +0100

    Update Controllers

--- YourPlace_BackEnd/YourPlace_BackEnd_App/src/main/java/Projects/YourPlace_BackEnd_App/controllers/PlaceController.java
+++ YourPlace_BackEnd/YourPlace_BackEnd_App/src/main/java/Projects/YourPlace_BackEnd_App/controllers/PlaceController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,9 +29,8 @@ public class PlaceController {
 	// - - - - - - createPlace - - - - - -
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Place createPlace() {
-		return null;
-
+	public Place createPlace(@RequestBody Place place) {
+		return placeService.createPlace(place);
 	}
 
 	// - - - - - - getPlaces - - - - - -
--- YourPlace_BackEnd/YourPlace_BackEnd_App/src/main/java/Projects/YourPlace_BackEnd_App/controllers/ReservationController.java
+++ YourPlace_BackEnd/YourPlace_BackEnd_App/src/main/java/Projects/YourPlace_BackEnd_App/controllers/ReservationController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,9 +28,8 @@ public class ReservationController {
 	// - - - - - - createReservation - - - - - -
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Reservation createReservation() {
-		return null;
-
+	public Reservation createReservation(@RequestBody Reservation reservation) {
+		return reservationService.createReservation(reservation);
 	}
 
 	// - - - - - - getReservations - - - - - -
