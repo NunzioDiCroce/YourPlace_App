4f04e6954e607a0c084724afbd30882e557492a0
NunzioDiCroce <nunziodavide@gmail.com>
2023-11-24 18:24:58 +0100

    Update Controllers

--- YourPlace_BackEnd/YourPlace_BackEnd_App/src/main/java/Projects/YourPlace_BackEnd_App/controllers/PlaceController.java
+++ YourPlace_BackEnd/YourPlace_BackEnd_App/src/main/java/Projects/YourPlace_BackEnd_App/controllers/PlaceController.java
@@ -1,18 +1,23 @@
 package Projects.YourPlace_BackEnd_App.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import Projects.YourPlace_BackEnd_App.entities.Place;
+import Projects.YourPlace_BackEnd_App.payloads.UpdatePlacePayload;
 import Projects.YourPlace_BackEnd_App.services.PlaceService;
 
 @RestController
@@ -35,30 +40,28 @@ public class PlaceController {
 
 	// - - - - - - getPlaces - - - - - -
 	@GetMapping("/places")
-	public Page<Place> getPlaces() {
-		return null;
-
+	public Page<Place> getPlaces(@RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "placeId") String sort) {
+		return placeService.getPlaces(page, size, sort);
 	}
 
 	// - - - - - - findPlace - - - - - -
 	@GetMapping("/{id}")
-	public Place findPlace() {
-		return null;
-
+	public Place findPlace(@PathVariable UUID id) {
+		return placeService.findPlace(id);
 	}
 
 	// - - - - - - updatePlace - - - - - -
 	@PutMapping("/{id}")
-	public Place updatePlace() {
-		return null;
-
+	public Place updatePlace(@PathVariable UUID id, @RequestBody UpdatePlacePayload payload) {
+		return placeService.updatePlace(id, payload);
 	}
 
 	// - - - - - - deletePlace - - - - - -
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void deletePlace() {
-
+	public void deletePlace(@PathVariable UUID id) {
+		placeService.deletePlace(id);
 	}
 
 }
--- YourPlace_BackEnd/YourPlace_BackEnd_App/src/main/java/Projects/YourPlace_BackEnd_App/controllers/ReservationController.java
+++ YourPlace_BackEnd/YourPlace_BackEnd_App/src/main/java/Projects/YourPlace_BackEnd_App/controllers/ReservationController.java
@@ -1,17 +1,22 @@
 package Projects.YourPlace_BackEnd_App.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import Projects.YourPlace_BackEnd_App.entities.Reservation;
+import Projects.YourPlace_BackEnd_App.payloads.UpdateReservationPayload;
 import Projects.YourPlace_BackEnd_App.services.ReservationService;
 
 @RestController
@@ -34,30 +39,28 @@ public class ReservationController {
 
 	// - - - - - - getReservations - - - - - -
 	@GetMapping("/reservations")
-	public Page<Reservation> getReservations() {
-		return null;
-
+	public Page<Reservation> getReservations(@RequestParam(defaultValue = "0") int page,
+			@RequestParam(defaultValue = "10") int size, @RequestParam(defaultValue = "reservationId") String sort) {
+		return reservationService.getReservations(page, size, sort);
 	}
 
 	// - - - - - - findReservation - - - - - -
 	@GetMapping("/{id}")
-	public Reservation findReservation() {
-		return null;
-
+	public Reservation findReservation(@PathVariable UUID id) {
+		return reservationService.findReservation(id);
 	}
 
 	// - - - - - - updateReservation - - - - - -
 	@GetMapping("/{id}")
-	public Reservation updateReservation() {
-		return null;
-
+	public Reservation updateReservation(@PathVariable UUID id, @RequestBody UpdateReservationPayload payload) {
+		return reservationService.updateReservation(id, payload);
 	}
 
 	// - - - - - - deleteReservation - - - - - -
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void deleteReservation() {
-
+	public void deleteReservation(@PathVariable UUID id) {
+		reservationService.deleteReservation(id);
 	}
 
 }
