12b07ff938cb259e81fcc212ff04951a84237fb8
NunzioDiCroce <nunziodavide@gmail.com>
2023-11-24 12:20:23 +0100

    Update Services

--- YourPlace_BackEnd/YourPlace_BackEnd_App/src/main/java/Projects/YourPlace_BackEnd_App/services/PlaceService.java
+++ YourPlace_BackEnd/YourPlace_BackEnd_App/src/main/java/Projects/YourPlace_BackEnd_App/services/PlaceService.java
@@ -1,8 +1,49 @@
 package Projects.YourPlace_BackEnd_App.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+
+import Projects.YourPlace_BackEnd_App.entities.Place;
+import Projects.YourPlace_BackEnd_App.repositories.PlaceRepository;
 
 @Service
 public class PlaceService {
+
+	private final PlaceRepository placeRepository;
+
+	@Autowired
+	public PlaceService(PlaceRepository placeRepository) {
+		this.placeRepository = placeRepository;
+	}
+
+	// - - - - - - createPlace - - - - - -
+	public Place createPlace() {
+		return null;
+
+	}
+
+	// - - - - - - getPlaces - - - - - -
+	public Page<Place> getPlaces() {
+		return null;
+
+	}
+
+	// - - - - - - findPlace - - - - - -
+	public Place findPlace() {
+		return null;
+
+	}
+
+	// - - - - - - updatePlace - - - - - -
+	public Place updatePlace() {
+		return null;
+
+	}
+
+	// - - - - - - deletePlace - - - - - -
+	public void deletePlace() {
+
+	}
 
 }
