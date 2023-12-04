e09ed089014927fdb79ed8f8ad89c95944697152
NunzioDiCroce <nunziodavide@gmail.com>
2023-12-04 09:44:48 +0100

    Update UserService.java

--- YourPlace_BackEnd/YourPlace_BackEnd_App/src/main/java/Projects/YourPlace_BackEnd_App/services/UserService.java
+++ YourPlace_BackEnd/YourPlace_BackEnd_App/src/main/java/Projects/YourPlace_BackEnd_App/services/UserService.java
@@ -1,10 +1,17 @@
 package Projects.YourPlace_BackEnd_App.services;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import Projects.YourPlace_BackEnd_App.entities.User;
+import Projects.YourPlace_BackEnd_App.exceptions.NotFoundException;
+import Projects.YourPlace_BackEnd_App.payloads.UpdateUserPayload;
 import Projects.YourPlace_BackEnd_App.repositories.UserRepository;
 
 @Service
@@ -20,30 +27,31 @@ public class UserService {
 	// - - - - - - createUser - - - - - -
 	public User createUser() {
 		return null;
-
 	}
 
 	// - - - - - - getUsers - - - - - -
-	public Page<User> getUsers() {
-		return null;
-
+	public Page<User> getUsers(int page, int size, String sort) {
+		return userRepository.findAll(PageRequest.of(page, size, Sort.by(sort)));
 	}
 
 	// - - - - - - findUser - - - - - -
-	public User findUser() {
-		return null;
-
+	public User findUser(UUID id) {
+		Optional<User> user = userRepository.findById(id);
+		return user.orElseThrow(() -> new NotFoundException("Item with id " + id + " not found."));
 	}
 
 	// - - - - - - updateUser - - - - - -
-	public User updateUser() {
-		return null;
-
+	public User updateUser(UUID id, UpdateUserPayload payload) {
+		User user = findUser(id);
+		user.setName(payload.getName());
+		user.setSurname(payload.getSurname());
+		return userRepository.save(user);
 	}
 
 	// - - - - - - deleteUser - - - - - -
-	public void deleteUser() {
-
+	public void deleteUser(UUID id) {
+		User user = findUser(id);
+		userRepository.delete(user);
 	}
 
 }
